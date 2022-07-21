1c55cfe1f9bd64cf17eae59e4e6799d5a1b8fcfd
Hoodoo42 <create@hoodooverse.ca>
2022-07-21 17:53:19 -0600

    working my way through the cookie warmup

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
-
+// setting a cookie with a value of 0
 Cookies.set(`clickCount`, 0);
 
+// this function will create a variable that will get(cookies.get) the value of clickCount
+// the parseInt is important, without it, cookies ONLY HOLD STRINGS and will keep adding the character `1` ie 1111111 (instead of 1+1 =2)
+// next line will allow that value to plus one each time
+// Cookies.set will reset the value for clickCount and hold the new value that currentCount will hold
 function countClicks(details){
-    let clicked = Cookies.get(`clickCount`)
-    console.log(`clickCount`);
-    // Cookies.set(`click_count`, 0)++;
-
+    let currentCount = parseInt(Cookies.get(`clickCount`));
+    currentCount++;
+    Cookies.set(`clickCount`, currentCount);
 }
-
+// creating an eventListener for when the targeted button is clicked it calls the above function
 let clicked = document.getElementById(`clickButton`);
 clicked.addEventListener (`click`, countClicks);
